afcd518141d880523ac6768c56bcd3df246c9851
omarkarios <markariosoketch@gmail.com>
2025-09-14 19:54:50 +0300

    Completed functions lab.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const { startTransition } = require("react");
+
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  const hq = 42;
+  let distanceInBlocks = Math.abs(block - hq);
+  return distanceInBlocks;
+}
+
+function distanceFromHqInFeet(block) {
+    const hq = 42;
+    let distanceInFeet = Math.abs(block - hq) * 264;
+    return distanceInFeet;
+}
+function distanceTravelledInFeet(start,destination) {
+    let distanceTravelled = Math.abs(start - destination) * 264;
+    return distanceTravelled;
+}
+ function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    let distanceTravelled = Math.abs(start - destination) * 264;
+    if(distanceTravelled <= 400){
+        return 0;
+    }
+    else if(distanceTravelled <= 2000){
+        return (distanceTravelled - 400) * 0.02;
+    }
+    else if(distanceTravelled <= 2500){
+        return 25;
+    }
+    else{
+        return "cannot travel that far";
+    }
+  }
